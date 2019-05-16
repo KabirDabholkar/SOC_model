fb88d08a82b1e45ccbb409bceeadfee5dde68f85
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-05-16 14:47:19 +0530

    running on cluster now

--- SOC_cluster2.py
+++ SOC_cluster2.py
@@ -6,15 +6,26 @@ import math
 import heapq
 import pickle as pkl
 import time
+import os
+import sys
 
-F=0.1
+
+
+F=0.01
+
+with open(os.getcwd()+"/outputloc.txt",'r') as f:
+    outputloc=f.read().replace('\n','')
 
 to_save={}
-save_loc="/home/kabir/SOC_model/data.pkl"
-save_loc="/storage/subhadra/kabir/output/SOC_model/data.pkl"
+save_loc=outputloc+"data.pkl"
+#save_loc="/storage/subhadra/kabir/output/SOC_model/data.pkl"
 
-time_loc="/home/kabir/SOC_model/time_2_"+str(F)+".txt"
+time_loc=outputloc+"time_2_"+str(F)+".txt"
 
+#time_loc="/storage/subhadra/kabir/output/SOC_model/time_2_"+str(F)+".txt"
+if os.path.isfile(time_loc):
+    os.remove(time_loc)
+########Functions###################################################### 
 def WRITE(text):
     with open(time_loc,'a') as time_file:
         time_file.write(text+"\n")
@@ -51,20 +62,25 @@ def my_kmax(R,k):
     return [i,j]
     
 
+WRITE("Networkx version:"+str(nx.__version__))
 
-WRITE("Starting")
-N = 2000
-f = 0.1 #f leak (fraction of leak)
+N = 10000
+f = F #f leak (fraction of leak)
 f = 1-f
-mean_degree = 4
+mean_degree = 4.0
 
 K = 4
-steps = 100
-#tic
+steps = 500
+
+WRITE("N="+str(N)+"\nf="+str(f)+"\nmean_degree="+str(mean_degree)+"\nK="+str(K)+"\nsteps="+str(steps))
+
+
 np.random.seed(1)
 G_undir = nx.erdos_renyi_graph(N,mean_degree/N)
+
 G_dir = nx.DiGraph()
 G_dir.add_nodes_from(range(N))
+
 G_dir.add_edges_from(G_undir.edges())
 
 
@@ -84,10 +100,9 @@ R = [(temp[2*i],temp[2*i+1]) for i in range(G_undir.number_of_edges())]
 recency={}
 for edge,rec in zip(G_dir.edges(),R):
     recency[edge]=rec
-nx.set_edge_attributes(G_dir,recency,name="recency")
-#R = temp.reshape(N,N)
-#R = np.multiply(R,A)
 
+nx.set_edge_attributes(G_dir,"recency",recency)
+#print(G_dir.edges(data=True))
 
 degree = np.array(np.sum(A,0))[0]
 G_undir.clear()
@@ -115,13 +130,13 @@ save_distribution = []
 WRITE("Starting simulation")
 start = time.time()
 for i in range(steps):
-    
+    WRITE("Step: "+str(i))
     if np.mod(i,float(steps)/50) == 0:
         dist = np.bincount(list(degree.flat))
         #x_axis = np.arange(0,max(degree)+1)
         save_distribution.append((dist,degree))
-    if np.mod(i,float(steps)/500) == 0:
-        WRITE("Step:"+str(i))
+    #if np.mod(i,float(steps)/500) == 0:
+        #WRITE("Step:"+str(i))
     #Particle addition
     
     add_site = np.random.randint(0,N-1)
@@ -155,19 +170,22 @@ for i in range(steps):
             else:
                 Ruse[c2,c1] = -1
     #"""
-    WRITE("Finding kmax indices")    
+    WRITE("Finding "+str(a)+"max indices")    
     if a!=0:
         edges=list(G_dir.edges())
+        
         R=[max(G_dir[edge[0]][edge[1]]['recency']) for edge in edges] #takes maximum of the two endpoints for each edge
         indices = np.array(heapq.nlargest(a, range(len(R)), R.__getitem__))
-        ie,je=tuple(zip(*[edges[i] for i in indices]))
+        
+        ie,je=tuple(zip(*[edges[ind] for ind in indices]))
     
     WRITE("Done kmax")    
     
     
-    WRITE("Starting Rewiring")   
+    WRITE("Starting Rewiring")
+   
     for j in range(a):
-        if (je[j] != add_site) & ((add_site,je[j]) not in G_dir.to_undirected().edges()):
+        if (je[j] != add_site) & (A[add_site,je[j]]!=0 or A[je[j],add_site]!=0):
             if (ie[j],je[j]) in G_dir.edges():
                 r=G_dir[ie[j]][je[j]]['recency'][1]
                 G_dir.remove_edge(ie[j],je[j])
@@ -182,16 +200,17 @@ for i in range(steps):
                 r=G_dir[edge[0]][edge[1]]['recency']
                 G_dir[edge[0]][edge[1]]['recency']=(r[0]+1,r[1]+1)
              
-    WRITE("Rewiring over\n"+str(i))
-    
+    WRITE("Rewiring over")
+    A = nx.to_numpy_matrix(G_dir.to_undirected(), dtype=np.int)
     
 end = time.time()
-WRITE("Iteration Time: "+str(end - start)+"\nStep:"+str(i))    
-#to_save['save_distribution']=save_distribution
+WRITE("Iteration Time: "+str(end - start))    
+to_save['save_distribution']=save_distribution
 
 #saving pickle
 
-#with open("/home/kabir/SOC_model/data.pkl","wb") as pickle_out:
-#    pkl.dump(to_save, pickle_out)
+with open("/home/kabir/SOC_model/data.pkl","wb") as pickle_out:
+    pkl.dump(save_loc, pickle_out)
+
 
 
